e31f7640199fc447b726563c51cf97cefaed9490
hppanpaliya <hppanpaliya@gmail.com>
2023-07-04 20:01:22 -0400

    added new route for getting all emails in DB

--- mailserver/emailRoutes.js
+++ mailserver/emailRoutes.js
@@ -12,7 +12,7 @@ router.get("/emails/:emailId", async (req, res) => {
     const { emailId } = req.params;
     console.log("emailId", emailId);
     const db = getDB();
-    const collection = db.collection(collectionName);
+    const collection = db.collection(emailId);
     const emails = await collection.find({ "to.text": emailId }).toArray();
 
     if (emails.length === 0) {
@@ -45,7 +45,7 @@ router.get("/emails-list/:emailId", async (req, res) => {
     const { emailId } = req.params;
     console.log("emailId", emailId);
     const db = getDB();
-    const collection = db.collection(collectionName);
+    const collection = db.collection(emailId);
     const emails = await collection
       .find(
         { "to.text": emailId.toLocaleLowerCase() },
@@ -63,13 +63,40 @@ router.get("/emails-list/:emailId", async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 });
+// Define the /all-emails route
 
-router.get("/email/:email_id", async (req, res) => {
+router.get("/all-emails", async (req, res) => {
+  try {
+    const db = getDB();
+    const collections = await db.listCollections().toArray();
+    const allEmails = [];
+
+    for (const collection of collections) {
+      const emails = await db
+        .collection(collection.name)
+        .find({}, { projection: { "from.text": 1, subject: 1, "to.value.address": 1, "from.value.address": 1 } })
+        .toArray();
+      allEmails.push(...emails);
+    }
+
+    if (allEmails.length === 0) {
+      return res.status(404).json({ message: "No emails found in the database" });
+    }
+
+    return res.json(allEmails);
+  } catch (error) {
+    console.error("Error retrieving emails:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/email/:emailID/:email_id", async (req, res) => {
   try {
     let { email_id } = req.params;
+    let { emailID } = req.params;
     console.log("email_id", email_id);
     const db = getDB();
-    const collection = db.collection(collectionName);
+    const collection = db.collection(emailID);
     email_id = new ObjectId(email_id);
 
     const emails = await collection.find({ _id: email_id }).toArray(); // fetch email data for input email uid
