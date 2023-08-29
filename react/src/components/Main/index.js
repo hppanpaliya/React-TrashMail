7e3377577a9b6b3a7e0e997912dc6ba13eb6f41a
hppanpaliya <hppanpaliya@gmail.com>
2023-08-29 18:52:54 -0400

    animation on Landing page

--- react/src/components/Main/index.js
+++ react/src/components/Main/index.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { Grid, Box, Typography } from "@mui/material";
 import ButtonSection from "../ButtonSection";
-import { useEffect, useState } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
-import { useContext } from "react";
+import { motion } from "framer-motion";
 
 const Main = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -23,7 +22,7 @@ const Main = () => {
   }, []);
 
   return (
-    <>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
       <Grid container spacing={2} sx={{ height: "100%" }}>
         <ButtonSection />
         <Grid
@@ -36,61 +35,68 @@ const Main = () => {
             textAlign: isMobile ? "center" : "left",
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
+          <motion.div initial={{ y: 50 }} animate={{ y: 0 }}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                marginTop: "5%",
+              }}
+            >
+              <Typography
+                variant="h2"
+                sx={{
+                  fontFamily: "Abhaya Libre SemiBold",
+                  fontSize: "1.3rem",
+                  color: darkMode ? "#fff" : "#000",
+                  opacity: "0.6",
+                }}
+              >
+                The Ultimate Solution for Disposable Email Addresses
+              </Typography>
+            </Box>
+          </motion.div>
 
-              marginTop: "5%",
-            }}
-          >
-            <Typography
-              variant="h2"
-              sx={{
-                fontFamily: "Abhaya Libre SemiBold",
-                fontSize: "1.3rem",
-                color: darkMode ? "#fff" : "#000",
-                opacity: "0.6",
-              }}
-            >
-              The Ultimate Solution for Disposable Email Addresses
-            </Typography>
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", marginTop: "1%" }}>
-            <Typography
-              variant="h2"
-              sx={{
-                margin: "0",
-                boxSizing: "border-box",
-                fontSize: isMobile ? "2.5rem" : "46.5pt",
-                fontFamily: "Abril Fatface",
-                color: darkMode ? "#fff" : "#000",
-                letterSpacing: "-1.24px",
-                textOverflow: "ellipsis",
-                overflow: "hidden",
-              }}
-            >
-              Welcome
-            </Typography>
-          </Box>
-          <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", marginTop: "3%" }}>
-            <Typography
-              variant="h2"
-              sx={{
-                fontFamily: "Abhaya Libre SemiBold",
-                fontSize: "1.3rem",
-                color: darkMode ? "#fff" : "#000",
-                opacity: "0.6",
-                maxWidth: isMobile ? "100%" : "36.5%",
-              }}
-            >
-              TrashMail is your go-to platform for creating temporary and disposable email addresses. With our cutting-edge technology, you can easily
-              protect your privacy and avoid spam in a hassle-free manner.
-            </Typography>
-          </Box>
+          <motion.div initial={{ scale: 0.7 }} animate={{ scale: 1 }}>
+            <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", marginTop: "1%" }}>
+              <Typography
+                variant="h2"
+                sx={{
+                  margin: "0",
+                  boxSizing: "border-box",
+                  fontSize: isMobile ? "2.5rem" : "46.5pt",
+                  fontFamily: "Abril Fatface",
+                  color: darkMode ? "#fff" : "#000",
+                  letterSpacing: "-1.24px",
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                }}
+              >
+                Welcome
+              </Typography>
+            </Box>
+          </motion.div>
+
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.5 }}>
+            <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", marginTop: "3%" }}>
+              <Typography
+                variant="h2"
+                sx={{
+                  fontFamily: "Abhaya Libre SemiBold",
+                  fontSize: "1.3rem",
+                  color: darkMode ? "#fff" : "#000",
+                  opacity: "0.6",
+                  maxWidth: isMobile ? "100%" : "36.5%",
+                }}
+              >
+                TrashMail is your go-to platform for creating temporary and disposable email addresses. With our cutting-edge technology, you can easily
+                protect your privacy and avoid spam in a hassle-free manner.
+              </Typography>
+            </Box>
+          </motion.div>
         </Grid>
       </Grid>
-    </>
+    </motion.div>
   );
 };
 
