2ac0f2b7bf7c44bd2ba1dffc0d4c1c4d9c91dc9c
hppanpaliya <hppanpaliya@gmail.com>
2026-01-17 22:38:27 -0500

    feat: add default environment configuration for API URL and domains

--- react/src/env.js
+++ react/src/env.js
@@ -1,1 +1,6 @@
-export const env = { ...process.env, ...window["env"] };
+const defaultEnv = {
+  REACT_APP_API_URL: '',
+  REACT_APP_DOMAINS: '["react-mail.com"]',
+};
+
+export const env = { ...defaultEnv, ...process.env, ...window["env"] };
